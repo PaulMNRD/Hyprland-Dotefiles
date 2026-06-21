f802c321b06a6e84684fa4d9e95cafd5feebd108
Paul Menard <paulmenard07104@gmail.com>
2026-06-21 10:11:16 +0200

    Better boot

--- modules/hosts/test-vm/configuration.nix
+++ modules/hosts/test-vm/configuration.nix
@@ -31,10 +31,32 @@
       ];
     };
 
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/vda";
-    boot.loader.grub.useOSProber = true;
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot = {
+      kernelPackages = pkgs.linuxPackages_latest;
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      loader.timeout = 0;
+      loader.grub = {
+        enable = true;
+        device = "/dev/vda";
+        # useOSProber = true;
+        timeoutStyle = "hidden";
+      };
+
+      kernelParams = [
+        "quiet"
+        "splash"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+
+      plymouth = {
+        enable = true;
+        theme = "breeze";
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       helix
